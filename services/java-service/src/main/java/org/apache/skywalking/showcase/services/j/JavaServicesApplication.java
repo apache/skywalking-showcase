72a89d6e450c173ae4fb09a9c4ec686698b48528
kezhenxu94 <kezhenxu94@apache.org>
2021-10-24 22:06:12 +0800

    Initialize

---
+++ services/java-service/src/main/java/org/apache/skywalking/showcase/services/j/JavaServicesApplication.java
@@ -0,0 +1,13 @@
+package org.apache.skywalking.showcase.services.j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaServicesApplication.class, args);
+	}
+
+}
---
+++ services/java-service/src/test/java/org/apache/skywalking/showcase/services/j/JavaServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.apache.skywalking.showcase.services.j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
