b61453fc5e4a0c43e65014463a22c7cfb3006f7e
kezhenxu94 <kezhenxu94@apache.org>
2021-10-27 21:57:04 +0800

    Set up gateway and songs service and deploy platform docker

---
+++ services/gateway-service/src/test/java/org/apache/skywalking/showcase/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.apache.skywalking.showcase.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
