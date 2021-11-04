a6c257b0c569e429fd8fc755aa36c6f9eaac5213
kezhenxu94 <kezhenxu94@apache.org>
2021-11-04 11:46:45 +0800

    Add usage for make help

--- scripts/make/help.mk
+++ scripts/make/help.mk
@@ -1,7 +1,23 @@
 ##@ Other targets
 
+define usage
+  You can customize the settings in Makefile.in
+  by specifying the variable in make command:
+
+  $$ make deploy.docker FEATURE_FLAGS=agent,vm
+
+  or via environment variable:
+
+  $$ export FEATURE_FLAGS=agent,vm && make deploy.docker
+endef
+
+export usage
+
 .PHONY: help
 help:  ## Display this help
+	@echo ''
+	@echo 'Usage:'
+	@echo "$$usage"
 	@awk 'BEGIN {FS = ":.*##"; printf "\nUsage:\n  make \033[36m<target>\033[0m\n"} \
 			/^[.a-zA-Z0-9_-]+:.*?##/ { printf "  \033[36m%-15s\033[0m \t%s\n", $$1, $$2 } \
 			/^##@/ { printf "\n\033[1m%s\033[0m\n", substr($$0, 5) }' $(MAKEFILE_LIST)
