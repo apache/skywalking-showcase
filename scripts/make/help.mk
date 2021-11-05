a368bf9edf773c3f130df49d2b7eb854d7b29e97
kezhenxu94 <kezhenxu94@apache.org>
2021-11-05 16:50:10 +0800

    Add Kubernetes deploy target and vm, so11y, highlight target

--- scripts/make/help.mk
+++ scripts/make/help.mk
@@ -1,3 +1,21 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
 ##@ Other targets
 
 define usage
@@ -27,7 +45,7 @@ features := $(subst $(comma), ,$(FEATURE_FLAGS))
 features := $(foreach f,$(features),@feature: *$(f)*)
 
 .PHONY: highlight
-highlight:  ## Highlight the important points of a feature
+highlight:  ## Highlight the important contents of a feature
 	@if [ "$(FEATURE_FLAGS)" = "" ]; then \
 	  echo 'FEATURE_FLAGS must be set in highlight target'; \
   	  exit 1; \
