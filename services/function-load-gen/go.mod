749e5c9c9721d9c358eefc87299e07b434478aa4
zhangwei <zhangwei24@apache.org>
2022-04-09 16:28:51 +0800

    Add feature function

---
+++ services/function-load-gen/go.mod
@@ -0,0 +1,18 @@
+module skywalking.apache.org/function-load-gen
+
+go 1.17
+
+require github.com/SkyAPM/go2sky v1.4.1
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/uuid v1.1.2 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
+	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20210624195500-8bfb893ecb84 // indirect
+	google.golang.org/grpc v1.40.0 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+	skywalking.apache.org/repo/goapi v0.0.0-20220401015832-2c9eee9481eb // indirect
+)
---
+++ services/function/main.go
@@ -0,0 +1,54 @@
+// Licensed to Apache Software Foundation (ASF) under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Apache Software Foundation (ASF) licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/OpenFunction/functions-framework-go/framework"
+	"github.com/OpenFunction/functions-framework-go/plugin"
+	"github.com/OpenFunction/functions-framework-go/plugin/skywalking"
+	"k8s.io/klog/v2"
+)
+
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %s!\n", r.URL.Path[1:])
+}
+
+func main() {
+	ctx := context.Background()
+	fwk, err := framework.NewFramework()
+	if err != nil {
+		klog.Fatal(err)
+	}
+	fwk.RegisterPlugins(map[string]plugin.Plugin{
+		"skywalking": &skywalking.PluginSkywalking{},
+	})
+
+	err = fwk.Register(ctx, HelloWorld)
+	if err != nil {
+		klog.Fatal(err)
+	}
+
+	err = fwk.Start(ctx)
+	if err != nil {
+		klog.Fatal(err)
+	}
+}
