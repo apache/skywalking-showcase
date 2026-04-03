67f73af57e47a532179e8b26ee71c485b399cb98
Wan Kai <wankai123@foxmail.com>
2026-04-03 10:39:34 +0800

    fix loadgen (#246)

--- services/load-gen/loadgen.py
+++ services/load-gen/loadgen.py
@@ -28,6 +28,9 @@ url = os.getenv('URL', 'http://frontend/index.html')
 
 options = ChromeOptions()
 options.add_argument("--headless")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-gpu")
 driver = webdriver.Chrome(options=options)
 
 while True:
